d259bfb37c0dc6c551f589e0b3c30094817c6d72
GABRIEL CAROTTI THEOTONIO RIOS <gabriel.ctr@puccampinas.edu.br>
2023-10-10 16:37:38 -0300

    adicionei algumas funções novas

--- testes.py
+++ testes.py
@@ -2,16 +2,13 @@
 
 import numpy as np
 import RobPy as rp
-import matplotlib.pyplot as plot
-from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-a = np.asarray([[1, 2, 3]]).T
-b =np.asarray([[4, 5, 6, 2]]).T
+R = rp.matriz_rotacao_y(77*np.pi/180)
+r = rp.cria_vetor3([1, 4, -3])
 
-# fig: plot.Figure = plot.figure()
-# ax = Axes3D(fig)
-# fig.add_axes(ax)
-# rp.plota_vetor3(a, color='m')
+T = rp.cria_operador4(m_rot_b_a=R)
 
-# //plot.show()
-print(rp.cria_vetor4(b))
+print(T)
+print(np.linalg.inv(T))
+
+print(-R.T @ r)
